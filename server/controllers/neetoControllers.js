25ede62d2969bca63818cabc1be4307c2959c2f2
Varun Sriram <s.varun@bigbinary.com>
2026-05-06 19:31:58 +0530

    Created a neetoControllers#consumerLogin action to redirect the consumer to Neeto product with the JWT

---
+++ server/controllers/neetoControllers.js
@@ -0,0 +1,22 @@
+const { isAuthorized } = require("../utils/auth-utils");
+const User = require("../models/User");
+const { default: NeetoJWT } = require("neeto-jwt");
+
+const NEETO_PRODUCTS = {
+  neetodesk: "https://spinkart.neetodesk.com/consumers/tickets",
+};
+
+exports.consumerLogin = async (req, res) => {
+  const { id, productId } = req.params;
+
+  if (!isAuthorized(id, req.session)) return res.sendStatus(403);
+
+  const user = await User.find(req.session.userId);
+  const redirectUri = NEETO_PRODUCTS[productId];
+  const loginUrl = new NeetoJWT({
+    email: user.username,
+    scope: "consumer",
+  }).generateLoginUrl(redirectUri);
+
+  res.redirect(loginUrl);
+};
