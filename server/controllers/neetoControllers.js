fd89fa8bff0f8939abab6048c652ccc0ce7a937b
Varun Sriram <s.varun@bigbinary.com>
2026-05-06 10:06:00 +0530

    Removed the redirect URI

--- server/controllers/neetoControllers.js
+++ server/controllers/neetoControllers.js
@@ -2,16 +2,23 @@ const { isAuthorized } = require("../utils/auth-utils");
 const User = require("../models/User");
 const { default: NeetoJWT } = require("neeto-jwt");
 
+const NEETO_PRODUCTS = {
+  neetocal: "http://spinkart.lvh.me:9000/admin/billing/neetocal/free-members",
+  neetorecord:
+    "http://spinkart.lvh.me:9000/admin/billing/neetorecord/free-members",
+};
+
 exports.login = async (req, res) => {
   const { id, productId } = req.params;
 
   if (!isAuthorized(id, req.session)) return res.sendStatus(403);
 
   const user = await User.find(req.session.userId);
-  const loginUrl = new NeetoJWT({ email: user.username }).generateLoginUrl(
-    productId,
-  );
-  console.log(loginUrl);
+  const redirectUri = NEETO_PRODUCTS[productId];
+  const loginUrl = new NeetoJWT({ email: user.username })
+    .generateLoginUrl
+    // redirectUri,
+    ();
 
   res.redirect(loginUrl);
 };
