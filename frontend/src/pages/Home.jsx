ac90145ce92ec7fbbb7f6e31838c9db0f2982ba6
Varun Sriram <s.varun@bigbinary.com>
2026-05-06 19:32:15 +0530

    Added a 'Go to NeetoDesk' button on the frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,22 @@
+import { useContext } from "react";
+import CurrentUserContext from "../contexts/current-user-context";
+
 export default function HomePage() {
-  return <>
-    <h1>Home</h1>
-    <p>Put something interesting here!</p>
-  </>
+  const { currentUser } = useContext(CurrentUserContext);
+  const isLoggedIn = !!currentUser?.id;
+
+  const neetoDeskUrl = `/api/neeto/consumer/${currentUser?.id}/neetodesk`;
+
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        {isLoggedIn && (
+          <a href={neetoDeskUrl} target="_blank">
+            Go to NeetoDesk
+          </a>
+        )}
+      </div>
+    </>
+  );
 }
