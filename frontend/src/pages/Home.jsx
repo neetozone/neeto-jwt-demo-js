7a08011eff2d97d341008d78a101d637281f6d62
Varun Sriram <s.varun@bigbinary.com>
2026-05-06 10:03:00 +0530

    Added a 'Go to NeetoCal' and 'Go to NeetoRecord' button on the frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,30 @@
+import { useContext } from "react";
+import CurrentUserContext from "../contexts/current-user-context";
+
 export default function HomePage() {
-  return <>
-    <h1>Home</h1>
-    <p>Put something interesting here!</p>
-  </>
+  const { currentUser } = useContext(CurrentUserContext);
+  const isLoggedIn = !!currentUser?.id;
+
+  const neetoCalUrl = `http://localhost:3000/api/neeto/${currentUser?.id}/neetocal`;
+  const neetoRecordUrl = `http://localhost:3000/api/neeto/${currentUser?.id}/neetorecord`;
+
+  return (
+    <>
+      <h1>Home</h1>
+      <div>
+        {isLoggedIn && (
+          <a href={neetoCalUrl} target="_blank">
+            Go to NeetoCal
+          </a>
+        )}
+      </div>
+      <div>
+        {isLoggedIn && (
+          <a href={neetoRecordUrl} target="_blank">
+            Go to NeetoRecord
+          </a>
+        )}
+      </div>
+    </>
+  );
 }
