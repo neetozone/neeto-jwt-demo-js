81ab34cacf907f1b533bba3aed0c71097da31382
Sojan Varghese <sojanvarghese@Sojans-MacBook-Air.local>
2026-05-11 19:28:02 +0530

    Updated workspace configuration to use environment variable from process sensitive file

--- server/controllers/neetoControllers.js
+++ server/controllers/neetoControllers.js
@@ -16,7 +16,7 @@ exports.consumerLogin = async (req, res) => {
   const loginUrl = new NeetoJWT({
     email: user.username,
     scope: "consumer",
-    workspace: "spinkart"
+    workspace: process.env.NEETO_JWT_WORKSPACE
   }).generateLoginUrl(redirectUri);
 
   res.redirect(loginUrl);
