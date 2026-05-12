217a482f45b9522901cc241162bca0c59df589f5
Sojan Varghese <sojanvarghese@Sojans-MacBook-Air.local>
2026-05-12 19:47:26 +0530

    Revert: Added workspace property to consumer login controller response

--- server/controllers/neetoControllers.js
+++ server/controllers/neetoControllers.js
@@ -1,14 +1,13 @@
+const { default: NeetoJWT } = require("neeto-jwt");
 const { isAuthorized } = require("../utils/auth-utils");
 const User = require("../models/User");
-const { default: NeetoJWT } = require("neeto-jwt");
 
 const NEETO_PRODUCTS = {
-  neetodesk: "https://spinkart.neetodesk.com/consumers/tickets",
+  neetodesk: `https://${process.env.NEETO_JWT_WORKSPACE}.neetodesk.com/consumers/tickets`,
 };
 
 exports.consumerLogin = async (req, res) => {
   const { id, productId } = req.params;
-
   if (!isAuthorized(id, req.session)) return res.sendStatus(403);
 
   const user = await User.find(req.session.userId);
@@ -16,7 +15,6 @@ exports.consumerLogin = async (req, res) => {
   const loginUrl = new NeetoJWT({
     email: user.username,
     scope: "consumer",
-    workspace: process.env.NEETO_JWT_WORKSPACE
   }).generateLoginUrl(redirectUri);
 
   res.redirect(loginUrl);
